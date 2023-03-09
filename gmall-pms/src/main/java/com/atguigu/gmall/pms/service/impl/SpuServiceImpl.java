b26d24880a56defcae8cd222af655011ecd35526
Guan FuQing <moumouguan@gmail.com>
2023-03-09 19:19:05 +0800

    Add Affairs: 2. 事务传播行为测试

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/SpuServiceImpl.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/SpuServiceImpl.java
@@ -28,6 +28,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -126,53 +128,55 @@ public class SpuServiceImpl extends ServiceImpl<SpuMapper, SpuEntity> implements
         return new PageResultVo(page);
     }
 
+    /**
+     * 方法截取快捷键
+     *      选中代码 ⌥ + ⌘ + m 输入方法名
+     *
+     * 事务的传播行为 @Transactional(propagation = Propagation.REQUIRES) 默认
+     *      一个 service 的方法 调用 另一个 service 的方法时 事务之间的影响 spring 特有
+     *
+     *      支持当前事务
+     *          REQUIRED    支持当前事务，如果不存在，就新建一个
+     *          SUPPORTS    支持当前事务，如果不存在，就不使用事务
+     *          MANDATORY   支持当前事务，如果不存在，抛出异常
+     *
+     *      不支持当前事务(挂起当前事务)
+     *          REQUIRES_NEW    如果有事务存在，挂起当前事务，创建一个新的事务
+     *          NOT_SUPPORTED   以非事务方式运行，如果有事务存在，挂起当前事务
+     *          NEVER           以非事务方式运行，如果有事务存在，抛出异常
+     *
+     *      嵌套事务
+     *          NESTED  如果当前事务存在，则嵌套事务执行（嵌套式事务）
+     *              依赖于JDBC3.0提供的SavePoint技术(保存点)
+     *              保存点不好控制所以不使用
+     *
+     *      常用的两种
+     *          REQUIRED：一个事务，要么成功，要么失败
+     *          REQUIRES_NEW：两个不同事务，彼此之间没有关系。一个事务失败了不影响另一个事务
+     *
+     * @param spu
+     */
+    @Transactional(propagation = Propagation.REQUIRED) // 事务注解 默认的 传播行为
     @Override
     public void bigSave(SpuVo spu) {
         // 1. 保存 spu 相关信息
         // 1.1 保存 spu 表
-        spu.setCreateTime(new Date());
-        // 再创建时间会导致不一致, 直接获取上一个设置的时间即可
-        spu.setUpdateTime(spu.getCreateTime());
-        save(spu);
-
-        // 保存完 spu 后主键回显 抽取 spuId 给以下保存方法使用
-        Long spuId = spu.getId();
+        Long spuId = saveSpuInfo(spu);
 
         // 1.2 保存 pms_spu_desc 本质与 spu 是同一张表(不需要批量新增使用 mapper 即可)
-        List<String> spuImages = spu.getSpuImages();
-        // spuImages 不为空才进行保存 spu 信息介绍表
-        if (CollectionUtils.isNotEmpty(spuImages)) {
-            SpuDescEntity spuDescEntity = new SpuDescEntity();
-            // 本质与 spu 是同一张表, 没有自己的 Id 需要 设置 spuId
-            spuDescEntity.setSpuId(spuId); // 设置图片 id
-            // 将集合以 "," 拼接符 拼接到一起形成新的 字符串
-            spuDescEntity.setDecript(StringUtils.join(spuImages, ",")); // ["1", "2"] -> "1,2"
-            descMapper.insert(spuDescEntity);
-        }
+        saveSpuDesc(spu, spuId);
+
+        // 传播行为演示
+        int i = 1 / 0;
 
         // 1.3 保存 pms_spu_attr_value 基本属性值表(需要使用批量保存使用 service)
-        List<SpuAttrValueVo> baseAttrs = spu.getBaseAttrs();
-        // baseAttrs 不为空才需要保存 spu 基本信息
-        if (CollectionUtils.isNotEmpty(baseAttrs)) {
-            baseAttrService.saveBatch(
-                    // 将 List<SpuAttrValueVo> 转换为 List<spuAttrValueEntity>
-                    baseAttrs.stream().map(spuAttrValueVo -> {
-                        SpuAttrValueEntity spuAttrValueEntity = new SpuAttrValueEntity();
-                        // 将 spuAttrValueVo 中的值赋值给 spuAttrValueEntity, 需要在设值前 拷贝, 否则会出现 数据丢失
-                        BeanUtils.copyProperties(spuAttrValueVo, spuAttrValueEntity); // 源 -> 对象(从 源中 拷贝到 对象)
-                        // 设置 spuId
-                        spuAttrValueEntity.setSpuId(spuId);
-                        // 设置排序字段
-                        spuAttrValueEntity.setSort(
-                                // 如果 spuAttrValueVo.getSort() 值为 null 则设置为 0
-                                Optional.ofNullable(spuAttrValueVo.getSort()).orElse(0)
-                        );
-                        return spuAttrValueEntity;
-                    }).collect(Collectors.toList())
-            );
-        }
+        saveBaseAttr(spu, spuId);
 
         // 2. 保存 sku 相关信息
+        saveSkuInfo(spu, spuId);
+    }
+
+    private void saveSkuInfo(SpuVo spu, Long spuId) {
         List<SkuVo> skus = spu.getSkus();
         // skus 不为 null 才进行保存
         if (CollectionUtils.isNotEmpty(skus)) {
@@ -235,6 +239,94 @@ public class SpuServiceImpl extends ServiceImpl<SpuMapper, SpuEntity> implements
                 smsClient.saveSales(skuSaleVo);
             });
         }
+    }
+
+    /**
+     * 1.3 保存 pms_spu_attr_value 基本属性值表(需要使用批量保存使用 service)
+     * @param spu
+     * @param spuId
+     */
+    private void saveBaseAttr(SpuVo spu, Long spuId) {
+        List<SpuAttrValueVo> baseAttrs = spu.getBaseAttrs();
+        // baseAttrs 不为空才需要保存 spu 基本信息
+        if (CollectionUtils.isNotEmpty(baseAttrs)) {
+            baseAttrService.saveBatch(
+                    // 将 List<SpuAttrValueVo> 转换为 List<spuAttrValueEntity>
+                    baseAttrs.stream().map(spuAttrValueVo -> {
+                        SpuAttrValueEntity spuAttrValueEntity = new SpuAttrValueEntity();
+                        // 将 spuAttrValueVo 中的值赋值给 spuAttrValueEntity, 需要在设值前 拷贝, 否则会出现 数据丢失
+                        BeanUtils.copyProperties(spuAttrValueVo, spuAttrValueEntity); // 源 -> 对象(从 源中 拷贝到 对象)
+                        // 设置 spuId
+                        spuAttrValueEntity.setSpuId(spuId);
+                        // 设置排序字段
+                        spuAttrValueEntity.setSort(
+                                // 如果 spuAttrValueVo.getSort() 值为 null 则设置为 0
+                                Optional.ofNullable(spuAttrValueVo.getSort()).orElse(0)
+                        );
+                        return spuAttrValueEntity;
+                    }).collect(Collectors.toList())
+            );
+        }
+    }
+
+    /**
+     * 1.2 保存 pms_spu_desc 本质与 spu 是同一张表(不需要批量新增使用 mapper 即可)
+     *
+     *      此处想演示的是, 当 saveSpuDesc 方法下出现异常时 saveSpuInfo 保存进行回滚, saveSpuDesc 开启新的事物 保存成功
+     *          Transactional 默认的传播行为 REQUIRES 支持当前事务，如果不存在，就新建一个.
+     *              需要手动设置　REQUIRES_NEW 如果有事务存在，挂起当前事务，创建一个新的事务.
+     *      测试:
+     *              1. bigSave() saveSpuDesc 方法下人为制作异常
+     *                  int i = 1 / 0;
+     *
+     *              2. bigSave() saveSpuDesc 保存成功不进行回滚 添加 REQUIRES_NEW 属性. 因为 private 不能被 增强 所以修改为 public
+     *                  @Transactional 是基于 aop 的, aop 需要对一个方法进行增强. 私有方法在外面无法被获取到, 切面类无法对该方法进行增强
+     *                      @Transactional是Spring框架提供的注解，用于在方法执行时开启事务管理。
+     *                      在Spring中，事务管理通常是由AOP来实现的，而AOP的实现原理是通过动态代理来生成代理对象，在代理对象中包含了对目标方法的增强逻辑。
+     *                      因此，如果将@Transactional注解加在private方法上，由于private方法无法被其他类调用，Spring框架无法生成代理对象，从而无法实现事务管理的功能。
+     *                      因此，@Transactional注解一般只能用在public或protected方法上，或者用在类级别上，以保证AOP可以正常工作。
+     *
+     *              3. jdk 代理(默认) 基于 接口代理. 接口中没有该方法无法进行增强, 所以 SpuService 扩展该方法
+     *                  因为 注解是 基于 AOP 的, AOP 是基于动态代理的. 动态代理分为两种 1. jdk 代理(默认) 基于 接口代理. 2. cglib 代理 基于类代理
+     *                      public void saveSpuDesc(SpuVo spu, Long spuId);
+     *
+     * 结果:
+     *      数据仍然进行回滚
+     *
+     * 原因: 事物的传播行为是(一个 service 的方法 调用 另一个 service 的方法时 事务之间的影响 spring 特有)
+     *      想要做到 saveBaseAttr 方法事物传播行为生效. 应该将方法放入 另一个 service 中. 在此处相当于 类调用方法 不是通过代理类调用. 事物注解没有生效, 传播行为更不可能生效
+     *
+     * @param spu
+     * @param spuId
+     */
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void saveSpuDesc(SpuVo spu, Long spuId) {
+        List<String> spuImages = spu.getSpuImages();
+        // spuImages 不为空才进行保存 spu 信息介绍表
+        if (CollectionUtils.isNotEmpty(spuImages)) {
+            SpuDescEntity spuDescEntity = new SpuDescEntity();
+            // 本质与 spu 是同一张表, 没有自己的 Id 需要 设置 spuId
+            spuDescEntity.setSpuId(spuId); // 设置图片 id
+            // 将集合以 "," 拼接符 拼接到一起形成新的 字符串
+            spuDescEntity.setDecript(StringUtils.join(spuImages, ",")); // ["1", "2"] -> "1,2"
+            descMapper.insert(spuDescEntity);
+        }
+    }
+
+    /**
+     * 1.1 保存 spu 表
+     * @param spu
+     * @return
+     */
+    private Long saveSpuInfo(SpuVo spu) {
+        spu.setCreateTime(new Date());
+        // 再创建时间会导致不一致, 直接获取上一个设置的时间即可
+        spu.setUpdateTime(spu.getCreateTime());
+        save(spu);
+
+        // 保存完 spu 后主键回显 抽取 spuId 给以下保存方法使用
+        Long spuId = spu.getId();
+        return spuId;
     }
 
 }
