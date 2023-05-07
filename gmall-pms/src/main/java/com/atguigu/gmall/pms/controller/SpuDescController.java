ae48ae7b70353b2fa5af5352ee60c812bc2d3820
Guan FuQing <moumouguan@gmail.com>
2023-05-07 14:35:05 +0800

    Add item: 数据导入 11. 根据 sku 中 spuId 查询 spu 的描述信息

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/SpuDescController.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/SpuDescController.java
@@ -1,23 +1,21 @@
 package com.atguigu.gmall.pms.controller;
 
-import java.util.List;
-
+import com.atguigu.gmall.common.bean.PageParamVo;
+import com.atguigu.gmall.common.bean.PageResultVo;
+import com.atguigu.gmall.common.bean.ResponseVo;
+import com.atguigu.gmall.pms.entity.SpuDescEntity;
+import com.atguigu.gmall.pms.service.SpuDescService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.atguigu.gmall.pms.entity.SpuDescEntity;
-import com.atguigu.gmall.pms.service.SpuDescService;
-import com.atguigu.gmall.common.bean.PageResultVo;
-import com.atguigu.gmall.common.bean.ResponseVo;
-import com.atguigu.gmall.common.bean.PageParamVo;
+import java.util.List;
 
 /**
  * spu信息介绍
@@ -48,6 +46,7 @@ public class SpuDescController {
 
     /**
      * 信息
+     * 11. 根据 sku 中 spuId 查询 spu 的描述信息
      */
     @GetMapping("{spuId}")
     @ApiOperation("详情查询")
