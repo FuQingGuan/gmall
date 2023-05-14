3ea46dcec33add7a3faa15dcbefb01da929eff31
Guan FuQing <moumouguan@gmail.com>
2023-05-14 18:39:07 +0800

    Add pay: 2. 调用支付宝接口打开支付页面同时记录对账信息

--- gmall-payment/src/main/java/com/atguigu/gmall/payment/mapper/PaymentMapper.java
+++ gmall-payment/src/main/java/com/atguigu/gmall/payment/mapper/PaymentMapper.java
@@ -1,11 +1,14 @@
 package com.atguigu.gmall.payment.mapper;
 
+import com.atguigu.gmall.payment.pojo.PaymentInfoEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
 /**
  * @Description:
  * @Author: Guan FuQing
  * @Date: 2023/5/14 13:35
  * @Email: moumouguan@gmail.com
  */
-public interface PaymentMapper {
+public interface PaymentMapper extends BaseMapper<PaymentInfoEntity> {
 
 }
